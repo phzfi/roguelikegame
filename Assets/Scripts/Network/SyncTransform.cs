9bdb93770ba59c0b2b66e394911ea43baa89740b
jtyot <ville.j.ollikainen@aalto.fi>
2015-10-28 21:27:10 +0200

    made custom script to run transform syncing

---
+++ Assets/Scripts/Network/SyncTransform.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class SyncTransform : NetworkBehaviour
+{
+
+    [SerializeField]
+    Transform myTransform;
+    [SerializeField]
+    float lerprate = 15;
+    [SyncVar]
+    Vector3 syncPosition;
+    [SyncVar]
+    Quaternion syncRotation;
+
+    void FixedUpdate()
+    {
+        TransmitTransform();
+        InterpolateTransform();
+    }
+
+    void InterpolateTransform()
+    {
+        if(!isLocalPlayer)
+        {
+            myTransform.position = Vector3.Lerp(myTransform.position, syncPosition, lerprate * Time.deltaTime);
+            myTransform.rotation = Quaternion.Slerp(myTransform.rotation, syncRotation, lerprate * Time.deltaTime);
+        }
+    }
+
+    [Command]
+    void CmdSendPosition(Vector3 pos)
+    {
+        syncPosition = pos;
+    }
+
+    [Command]
+    void CmdSendRotation(Quaternion rot)
+    {
+        syncRotation = rot;
+    }
+
+    [ClientCallback]
+    void TransmitTransform()
+    {
+        if (isLocalPlayer)
+        {
+            CmdSendPosition(myTransform.position);
+            CmdSendRotation(myTransform.rotation);
+        }
+    }
+}
