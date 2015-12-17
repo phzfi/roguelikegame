bcf772f13176940df7983fa33dff54e95ccf2cb7
Jussi Toivonen <jussittoivo@gmail.com>
2015-12-17 12:57:36 +0200

    Some more tests

--- Assets/Scripts/Editor/UnitTests.cs
+++ Assets/Scripts/Editor/UnitTests.cs
@@ -9,6 +9,14 @@ public class UnitTests
     GameObject testPlayer = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
     [Test]
+    public void TestInventory()
+    {
+        Inventory inv = new Inventory();
+        Assert.That(inv != null);
+    }
+
+
+    [Test]
     public void TestOneItemPickup()
     {
         var inventory = testPlayer.AddComponent<Inventory>();
@@ -30,6 +38,31 @@ public class UnitTests
             inventory.AddItem(itemToAdd);
         }
         Assert.That(!inventory.CanAddItem(itemToAdd));
+    }
+
+    [Test]
+    public void TestPlayerTakeDamage()
+    {
+        var combatSystem = testPlayer.AddComponent<CombatSystem>();
+
+        int hp = 5;
+
+        combatSystem.m_currentHp = hp;
+
+        combatSystem.GetHit(2);
+
+        Assert.That(combatSystem.m_currentHp == hp - 2);
+    }
+
+    [Test]
+    public void TestPlayerDie()
+    {
+        var combatSystem = testPlayer.AddComponent<CombatSystem>();
+
+        int hp = 5;
+        combatSystem.m_currentHp = hp;
+        combatSystem.GetHit(5);
+        Assert.That(testPlayer.activeInHierarchy);
     }
 
 }
