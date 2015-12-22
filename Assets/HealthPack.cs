1a875a0c53ef24d119307a4d46ea1c5612d72f9f
Toivonen Jussi <toivonj6@niksula.hut.fi>
2015-12-22 12:58:52 +0200

    Base for action bar and a simple health pack

---
+++ Assets/HealthPack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class HealthPack : MonoBehaviour
+{
+
+    public int m_heals = 1;
+
+    public void UseHealthPack()
+    {
+        var player = CharManager.GetLocalPlayer();
+        var combatSystem = player.GetComponent<CombatSystem>();
+        combatSystem.m_currentHp = Mathf.Min(combatSystem.m_maxHp, combatSystem.m_currentHp + m_heals);
+        Destroy(gameObject);
+    }
+}
