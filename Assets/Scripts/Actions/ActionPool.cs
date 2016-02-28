44763e315bfbfabe0ac0a48f8a9071ca88dad8ac
jtyot <ville.j.ollikainen@aalto.fi>
2016-02-28 21:52:14 +0200

    added missing files

---
+++ Assets/Scripts/Actions/ActionPool.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class ActionPool : NetworkBehaviour {
+	private uint m_firstFreeIndex = 0;
+
+	[SyncVar]
+	private uint m_objectID = 0;
+	private static uint sm_lastID = 0;
+	[SyncVar]
+	private bool initialized = false;
+
+	void Start()
+	{
+		Initialize();
+	}
+
+	public void Initialize()
+	{
+		if (!initialized)
+		{
+			m_objectID = sm_lastID++;
+			initialized = true;
+		}
+	}
+	
+	public ulong GetNextFreeID() // pack object ID and action ID into a single unique ID for an action.
+	{
+		return (((ulong)(m_objectID) << 32) | (m_firstFreeIndex++));
+	}
+}
